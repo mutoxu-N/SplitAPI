7a80daa24b5457f9773cf9d2110b939e8516c6b9
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2024-05-27 13:14:46 +0900

    POSTリクエストの処理

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,3 +7,15 @@ app = FastAPI()
 @app.get("/hello")
 async def hello(param: str = None):
     return {"message": "Hello World", "param": param.upper()}
+
+
+class Hello(BaseModel):
+    message: str
+    param: str
+
+
+@app.post("/write")
+async def write(request: Request, hello: Hello):
+    print(request.headers['token'])
+    print(hello)
+    return {"message": "Write", "param": f"Hello(mes={hello.message}, param={hello.param})"}
