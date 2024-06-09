37820ee50602deda98bec230d5c7ec5e8c952841
mutoxu=N <55544957+mutoxu-N@users.noreply.github.com>
2024-06-09 21:09:31 +0900

    /room/create の実装

--- firebase.py
+++ firebase.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from firebase_admin import auth
-from models import Role
+from models import Role, Settings
 
 # サービスアカウントでログイン
 cred = credentials.Certificate("firebase_secret.json")
@@ -93,3 +93,32 @@ class FirebaseApi:
         role = db.collection("rooms").document(self.room_id).collection(
             "members").document(name).get(["role"]).to_dict()["role"]
         return Role.of(role)
+
+    def check_if_room_exists(self) -> bool:
+        db = firestore.client()
+        room = db.collection("rooms").document(self.room_id).get()
+        if room.exists:
+            return True
+        else:
+            return False
+
+    def create_room(self, settings: Settings, owner_name: str) -> None:
+        db = firestore.client()
+        db.collection("rooms").document(self.room_id).set({
+            "settings": {
+                "split_unit": settings.split_unit,
+                "permission_receipt_create": settings.permission_receipt_create,
+                "permission_receipt_create": settings.permission_receipt_edit,
+                "on_new_member_request": settings.on_new_member_request,
+                "accept_rate": settings.accept_rate,
+            },
+            "users": {
+                self.uid: owner_name,
+            },
+        })
+        db.collection("rooms").document(self.room_id).collection("members").add({
+            "name": owner_name,
+            "id": self.uid,
+            "weight": 1.0,
+            "role": "OWNER",
+        }, owner_name)
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+from models import Settings
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from firebase import FirebaseApi
 
+import random
+import string
+
 import os
 os.environ["FIRESTORE_EMULATOR_HOST"] = "localhost:8080"
 os.environ["FIREBASE_AUTH_EMULATOR_HOST"] = "localhost:9099"
+
+
+# consts
+LENGTH_ROOM_ID = 6
 
 app = FastAPI()
 
@@ -23,6 +31,21 @@ async def reset(request: Request):
 
 
 @app.post("/room/create")
-async def room_create(request: Request):
-    print(request.body)
-    return {"room_id": "1AB23C"}
+async def room_create(request: Request, settings: Settings):
+    # 新しいルームIDの作成
+    def gen_room_id() -> str:
+        chars = [random.choice(string.ascii_uppercase + string.digits)
+                 for _ in [None]*LENGTH_ROOM_ID]
+        return "".join(chars)
+
+    id_not_generated = True
+    api: FirebaseApi = None
+    while id_not_generated:
+        room_id = gen_room_id()
+        api = FirebaseApi(request.headers['token'], room_id)
+        id_not_generated = api.check_if_room_exists()
+
+    # 生成したルームIDでルームの初期設定を行う
+    api.create_room(settings, request.headers['name'])
+
+    return {"room_id": room_id}
